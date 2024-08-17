0ceeec209759278caa4491a4cba94215cdd086be
akchen1 <43446512+akchen1@users.noreply.github.com>
2024-08-17 16:49:06 -0600

    add wall layer to level 3

--- Assets/GameAssets/Scripts/Interfaces/IBounceable.cs
+++ Assets/GameAssets/Scripts/Interfaces/IBounceable.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Objects implementing this can bounce on a bouncing platform
+ */
 public interface IBounceable
 {
     public float forceMultiplier { get { return 1.0f; } }
