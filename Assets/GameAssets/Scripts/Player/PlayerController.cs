e46a6ea9c9434dcb68eab75d0278311b14ac9128
Gaibryol <gaibryol@gmail.com>
2024-08-17 16:39:01 -0600

    basic movement

--- Assets/GameAssets/Scripts/Player/PlayerController.cs
+++ Assets/GameAssets/Scripts/Player/PlayerController.cs
@@ -13,72 +13,341 @@ public class PlayerController : MonoBehaviour
 	[SerializeField, Header("Stats")] private float movespeed;
 
 	private Rigidbody2D rbody;
-	private CircleCollider2D coll;
-
-	private Collision2D currentWall;
+	private Constants.Player.Inputs lastInput;
 
 	private readonly EventBrokerComponent eventBroker = new EventBrokerComponent();
 
     void Start()
     {
 		rbody = GetComponent<Rigidbody2D>();
-		coll = GetComponent<CircleCollider2D>();
+		lastInput = Constants.Player.Inputs.None;
     }
 
     void FixedUpdate()
     {
-		//rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+		RaycastHit2D rightHit = Physics2D.Raycast(transform.position, Vector2.right, Constants.Player.RaycastDistance, 1 << LayerMask.NameToLayer("Wall"));
+		RaycastHit2D leftHit = Physics2D.Raycast(transform.position, Vector2.left, Constants.Player.RaycastDistance, 1 << LayerMask.NameToLayer("Wall"));
+		//RaycastHit2D topHit = Physics2D.Raycast(transform.position, Vector2.up, Constants.Player.RaycastDistance, 1 << LayerMask.NameToLayer("Wall"));
+		RaycastHit2D bottomHit = Physics2D.Raycast(transform.position, Vector2.down, Constants.Player.RaycastDistance, 1 << LayerMask.NameToLayer("Wall"));
+		RaycastHit2D bottomRightHit = Physics2D.Raycast(transform.position, new Vector2(1f, -1f), Constants.Player.DiagonalRaycastDistance, 1 << LayerMask.NameToLayer("Wall"));
+		RaycastHit2D bottomLeftHit = Physics2D.Raycast(transform.position, new Vector2(-1f, -1f), Constants.Player.DiagonalRaycastDistance, 1 << LayerMask.NameToLayer("Wall"));
+		RaycastHit2D topRightHit = Physics2D.Raycast(transform.position, new Vector2(1f, 1f), Constants.Player.DiagonalRaycastDistance, 1 << LayerMask.NameToLayer("Wall"));
+		RaycastHit2D topLeftHit = Physics2D.Raycast(transform.position, new Vector2(-1f, 1f), Constants.Player.DiagonalRaycastDistance, 1 << LayerMask.NameToLayer("Wall"));
 
-		if (currentWall == null)
+		//Debug.Log("Right: " + rightHit.collider);
+		//Debug.Log("Left: " + leftHit.collider);
+		//Debug.Log("Top: " + topHit.collider);
+		//Debug.Log("Bottom: " + bottomHit.collider);
+		//Debug.Log("Bottom Right: " + bottomRightHit.collider);
+		//Debug.Log("Bottom Left: " + bottomLeftHit.collider);
+
+		if (rightHit.collider != null && leftHit.collider == null && bottomHit.collider == null)
 		{
+			// Wall on right
+			rbody.velocityX = 0f;
+			rbody.velocityY = move.ReadValue<float>() * movespeed * Time.deltaTime;
+			rbody.gravityScale = 0f;
+		}
+		else if (rightHit.collider == null && leftHit.collider != null && bottomHit.collider == null)
+		{
+			// Wall on left
+			rbody.velocityX = 0f;
+			rbody.velocityY = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+			rbody.gravityScale = 0f;
+		}
+		//else if (rightHit.collider == null && leftHit.collider == null && topHit.collider == null && bottomHit.collider == null)
+		//{
+		//	// Wall on top
+		//	rbody.velocityX = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//	rbody.gravityScale = 0f;
+		//}
+		else if (rightHit.collider == null && leftHit.collider == null && bottomHit.collider != null)
+		{
+			// Wall on bottom
 			rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
 			rbody.gravityScale = 1f;
 		}
-		else if (Vector2.Dot(currentWall.transform.position - transform.position, transform.right) > 0)
+		else if (rightHit.collider != null && bottomHit.collider != null)
 		{
-			rbody.gravityScale = 0f;
-
-			if (move.ReadValue<float>() < 0)
+			// Wall on right and bottom
+			if (lastInput == Constants.Player.Inputs.A)
 			{
-				// Climb down wall
-				rbody.velocityY = Mathf.Abs(move.ReadValue<float>()) * movespeed * Time.deltaTime;
+				// Was coming down wall, move off wall
+				rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+				rbody.gravityScale = 1f;
 			}
-			else if (move.ReadValue<float>() > 0)
+			else if (lastInput == Constants.Player.Inputs.D)
 			{
-				// Climb up wall
-				rbody.velocityY = Mathf.Abs(move.ReadValue<float>()) * -movespeed * Time.deltaTime;
-			}
-			else
-			{
-				// Stop
-				rbody.velocityY = 0f;
+				// Was moving towards wall, go up wall
+				rbody.velocityY = move.ReadValue<float>() * movespeed * Time.deltaTime;
+				rbody.gravityScale = 0f;
 			}
 		}
-		else if (Vector2.Dot(currentWall.transform.position - transform.position, transform.right) < 0)
+		else if (leftHit.collider != null && bottomHit.collider != null)
 		{
-			rbody.gravityScale = 0f;
-
-			if (move.ReadValue<float>() < 0)
+			// Wall on left and bottom
+			if (lastInput == Constants.Player.Inputs.A)
 			{
-				// Climb up wall
-				rbody.velocityY = Mathf.Abs(move.ReadValue<float>()) * -movespeed * Time.deltaTime;
+				// Was moving towards wall, go up wall
+				rbody.velocityY = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+				rbody.gravityScale = 0f;
 			}
-			else if (move.ReadValue<float>() > 0)
+			else if (lastInput == Constants.Player.Inputs.D)
 			{
-				// Climb down wall
-				rbody.velocityY = Mathf.Abs(move.ReadValue<float>()) * movespeed * Time.deltaTime;
+				// Was coming down wall, move off wall
+				rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+				rbody.gravityScale = 1f;
+			}
+		}
+		//else if (leftHit.collider != null && topHit.collider != null)
+		//{
+		//	// Wall on left and top
+		//	if (lastInput == Constants.Player.Inputs.A)
+		//	{
+		//		// Was moving up wall, go to ceiling
+		//		rbody.velocityX = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//		rbody.gravityScale = 0f;
+		//	}
+		//	else if (lastInput == Constants.Player.Inputs.D)
+		//	{
+		//		// Move down the wall
+		//		rbody.velocityY = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//		rbody.gravityScale = 0f;
+		//	}
+		//}
+		//else if (rightHit.collider != null && topHit.collider != null)
+		//{
+		//	// Wall on right and top
+		//	if (lastInput == Constants.Player.Inputs.A)
+		//	{
+		//		// Mode down the wall
+		//		rbody.velocityY = move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//		rbody.gravityScale = 0f;
+		//	}
+		//	else if (lastInput == Constants.Player.Inputs.D)
+		//	{
+		//		// Was moving up wall, go to ceiling
+		//		rbody.velocityX = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//		rbody.gravityScale = 0f;
+
+		//	}
+		//}
+		else if (rightHit.collider == null && bottomHit.collider == null && bottomRightHit.collider != null)
+		{
+			if (Mathf.Abs(bottomRightHit.normal.x) == 1f)
+			{
+				// Moving on vertical wall
+				if (lastInput == Constants.Player.Inputs.A)
+				{
+					// Going over edge towards the left
+					rbody.velocityX = 0;
+					rbody.velocityY = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
+				else if (lastInput == Constants.Player.Inputs.D)
+				{
+					// Going over corner towards the right
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.velocityY = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
+			}
+			else if (Mathf.Abs(bottomRightHit.normal.y) == 1f)
+			{
+				if (lastInput == Constants.Player.Inputs.A)
+				{
+					// Still moving towards the left
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.velocityY = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
+				else if (lastInput == Constants.Player.Inputs.D)
+				{
+					// Going over corner towards the right
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
 			}
 			else
 			{
-				// Stop
-				rbody.velocityY = 0f;
+				if (move.ReadValue<float>() < 0f)
+				{
+					// Moving down a ramp on the left
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.velocityY = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
+				else if (move.ReadValue<float>() > 0f)
+				{
+					// Moving up a ramp on the right
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
+				else
+				{
+					// Not moving on ramp
+					rbody.velocity = Vector2.zero;
+					rbody.gravityScale = 0f;
+				}
 			}
+		}
+		else if (leftHit.collider == null && bottomHit.collider == null && bottomLeftHit.collider != null)
+		{
+			if (Mathf.Abs(bottomLeftHit.normal.x) == 1f)
+			{
+				// Moving on vertical wall
+				if (lastInput == Constants.Player.Inputs.A)
+				{
+					// Going over corner towards the left
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.velocityY = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
+				else if (lastInput == Constants.Player.Inputs.D)
+				{
+					// Going over edge towards the left
+					rbody.velocityX = 0;
+					rbody.velocityY = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
+			}
+			else if (Mathf.Abs(bottomLeftHit.normal.y) == 1f)
+			{
+				if (lastInput == Constants.Player.Inputs.A)
+				{
+					// Going over corner towards the left
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+
+					
+				}
+				else if (lastInput == Constants.Player.Inputs.D)
+				{
+					// Still moving towards the right
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.velocityY = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
+			}
+			else
+			{
+				if (move.ReadValue<float>() < 0f)
+				{
+					// Moving up a ramp on the left
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+					
+				}
+				else if (move.ReadValue<float>() > 0f)
+				{
+					// Moving down a ramp on the right
+					rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+					rbody.velocityY = -Mathf.Abs(move.ReadValue<float>()) * movespeed * Time.deltaTime;
+					rbody.gravityScale = 1f;
+				}
+				else
+				{
+					// Not moving on ramp
+					rbody.velocity = Vector2.zero;
+					rbody.gravityScale = 0f;
+				}
+			}
+		}
+		//else if (leftHit.collider == null && topHit.collider == null && topLeftHit.collider != null)
+		//{
+		//	if (lastInput == Constants.Player.Inputs.A)
+		//	{
+		//		// Go around the corner
+		//		rbody.velocityX = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//		rbody.velocityY = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//		rbody.gravityScale = 0f;
+		//	}
+		//	else if (lastInput == Constants.Player.Inputs.D)
+		//	{
+		//		// Going away from corner
+		//		rbody.velocityX = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//		rbody.gravityScale = 0f;
+		//	}
+		//}
+		//else if (rightHit.collider == null && topHit.collider == null && topRightHit.collider != null)
+		//{
+		//	if (Mathf.Abs(topRightHit.normal.x) == 1f)
+		//	{
+		//		// Moving on vertical wall
+		//		if (lastInput == Constants.Player.Inputs.A)
+		//		{
+		//			// Going away from corner
+		//			rbody.velocityX = 0f;
+		//			rbody.velocityY = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//			rbody.gravityScale = 0f;
+		//		}
+		//		else if (lastInput == Constants.Player.Inputs.D)
+		//		{
+		//			// Going over edge towards up
+		//			rbody.velocityX = 0;
+		//			rbody.velocityY = move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//			rbody.gravityScale = 0f;
+		//		}
+		//	}
+		//	else if (Mathf.Abs(topRightHit.normal.y) == 1f)
+		//	{
+		//		// Moving on horizontal wall
+		//		if (lastInput == Constants.Player.Inputs.A)
+		//		{
+		//			// Going away from corner
+		//			rbody.velocityX = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//			rbody.gravityScale = 0f;
+		//		}
+		//		else if (lastInput == Constants.Player.Inputs.D)
+		//		{
+		//			// Go around the corner
+		//			rbody.velocityX = -move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//			rbody.velocityY = move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//			rbody.gravityScale = 0f;
+		//		}
+		//	}
+		//	else
+		//	{
+		//		if (move.ReadValue<float>() < 0f)
+		//		{
+		//			// Moving up a ramp on the right
+		//			rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//			rbody.velocityY = -Mathf.Abs(move.ReadValue<float>()) * movespeed * Time.deltaTime;
+		//			rbody.gravityScale = 0f;
+
+		//		}
+		//		else if (move.ReadValue<float>() > 0f)
+		//		{
+		//			// Moving down a ramp on the right
+		//			rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+		//			rbody.gravityScale = 0f;
+		//		}
+		//		else
+		//		{
+		//			// Not moving on ramp
+		//			rbody.velocity = Vector2.zero;
+		//			rbody.gravityScale = 0f;
+		//		}
+		//	}
+		//}
+		else if (rightHit.collider == null && leftHit.collider == null && bottomHit.collider == null && bottomRightHit.collider == null && bottomLeftHit.collider == null)
+		{
+			// Freefall, allow for air strafe
+			rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+			rbody.gravityScale = 1f;
 		}
 	}
 
 	public void OnMove(InputAction.CallbackContext context)
 	{
-		Debug.Log("Move: " + context.ReadValue<float>());
+		if (context.ReadValue<float>() > 0)
+		{
+			lastInput = Constants.Player.Inputs.D;
+		}
+		else if (context.ReadValue<float>() < 0)
+		{
+			lastInput = Constants.Player.Inputs.A;
+		}
 	}
 
 	public void OnDrop(InputAction.CallbackContext context)
@@ -94,22 +363,6 @@ public class PlayerController : MonoBehaviour
 	public void OnInteract(InputAction.CallbackContext context)
 	{
 		Debug.Log("Interact");
-	}
-
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-		if (collision.gameObject.tag == "Wall")
-		{
-			currentWall = collision;
-		}
-	}
-
-	private void OnCollisionExit2D(Collision2D collision)
-	{
-		if (collision.gameObject.tag == "Wall")
-		{
-			currentWall = null;
-		}
 	}
 
 	private void OnEnable()
