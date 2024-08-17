837ccfc1fa96c855c29c89da5ab01aad4cc67962
Gaibryol <gaibryol@gmail.com>
2024-08-16 22:51:32 -0600

    more player movement stuff

--- Assets/GameAssets/Scripts/Player/PlayerController.cs
+++ Assets/GameAssets/Scripts/Player/PlayerController.cs
@@ -10,23 +10,75 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private InputAction split;
 	[SerializeField] private InputAction interact;
 
+	[SerializeField, Header("Stats")] private float movespeed;
+
+	private Rigidbody2D rbody;
+	private CircleCollider2D coll;
+
+	private Collision2D currentWall;
+
 	private readonly EventBrokerComponent eventBroker = new EventBrokerComponent();
 
-    // Start is called before the first frame update
     void Start()
     {
-
+		rbody = GetComponent<Rigidbody2D>();
+		coll = GetComponent<CircleCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
+		//rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+
+		if (currentWall == null)
+		{
+			rbody.velocityX = move.ReadValue<float>() * movespeed * Time.deltaTime;
+			rbody.gravityScale = 1f;
+		}
+		else if (Vector2.Dot(currentWall.transform.position - transform.position, transform.right) > 0)
+		{
+			rbody.gravityScale = 0f;
+
+			if (move.ReadValue<float>() < 0)
+			{
+				// Climb down wall
+				rbody.velocityY = Mathf.Abs(move.ReadValue<float>()) * movespeed * Time.deltaTime;
+			}
+			else if (move.ReadValue<float>() > 0)
+			{
+				// Climb up wall
+				rbody.velocityY = Mathf.Abs(move.ReadValue<float>()) * -movespeed * Time.deltaTime;
+			}
+			else
+			{
+				// Stop
+				rbody.velocityY = 0f;
+			}
+		}
+		else if (Vector2.Dot(currentWall.transform.position - transform.position, transform.right) < 0)
+		{
+			rbody.gravityScale = 0f;
+
+			if (move.ReadValue<float>() < 0)
+			{
+				// Climb up wall
+				rbody.velocityY = Mathf.Abs(move.ReadValue<float>()) * -movespeed * Time.deltaTime;
+			}
+			else if (move.ReadValue<float>() > 0)
+			{
+				// Climb down wall
+				rbody.velocityY = Mathf.Abs(move.ReadValue<float>()) * movespeed * Time.deltaTime;
+			}
+			else
+			{
+				// Stop
+				rbody.velocityY = 0f;
+			}
+		}
+	}
 
 	public void OnMove(InputAction.CallbackContext context)
 	{
-		Debug.Log("Move");
+		Debug.Log("Move: " + context.ReadValue<float>());
 	}
 
 	public void OnDrop(InputAction.CallbackContext context)
@@ -42,6 +94,22 @@ public class PlayerController : MonoBehaviour
 	public void OnInteract(InputAction.CallbackContext context)
 	{
 		Debug.Log("Interact");
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Wall")
+		{
+			currentWall = collision;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Wall")
+		{
+			currentWall = null;
+		}
 	}
 
 	private void OnEnable()
