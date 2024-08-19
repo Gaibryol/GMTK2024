244a62e1ab88aeb17feebb949233289a838c4b56
akchen1 <43446512+akchen1@users.noreply.github.com>
2024-08-19 01:07:55 -0600

    add foreground lv 4 lv 5

---
+++ Assets/GameAssets/Scripts/CircleWipe.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static StarEvents;
+using static UnityEditor.Experimental.GraphView.Port;
+
+public class CircleWipe : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Camera mainCam;
+
+    private Material material;
+    private int fillAmountID = Shader.PropertyToID("_fillAmount");
+    private int offsetID = Shader.PropertyToID("_offset");
+
+    private readonly EventBrokerComponent eventBroker = new EventBrokerComponent();
+
+    private bool active = false;
+    private float fillAmount = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Image>().material;
+        material.SetFloat(fillAmountID, fillAmount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!active) return;
+        TrackTarget();
+
+        fillAmount = Mathf.MoveTowards(fillAmount, 0.07f, Time.deltaTime * 10f);
+        material.SetFloat(fillAmountID, fillAmount);
+
+    }
+
+    private void OnEnable()
+    {
+        eventBroker.Subscribe<LevelEvents.EndLevel>(OnLevelEnd);
+    }
+
+
+    private void OnDisable()
+    {
+        eventBroker.Unsubscribe<LevelEvents.EndLevel>(OnLevelEnd);
+    }
+
+    private void OnLevelEnd(BrokerEvent<LevelEvents.EndLevel> @event)
+    {
+        if (@event.Payload.Victory)
+        {
+            //active = true;
+        }
+    }
+
+    private void TrackTarget()
+    {
+
+        Vector3 targetPosition = target.transform.position;
+        Vector3 cameraPoint = mainCam.WorldToScreenPoint(targetPosition);
+        Vector2 uiPoint = cameraPoint / new Vector2(Screen.width, Screen.height);
+        material.SetVector(offsetID, uiPoint);
+    }
+
+}
